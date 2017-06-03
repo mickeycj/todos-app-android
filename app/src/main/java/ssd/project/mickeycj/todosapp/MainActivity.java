e65a798bf1c2894aee6ce9c85db7a3dabf7bfda5
Chanon Jenakom <chanonjenakom@gmail.com>
2017-06-03 21:14:00 +0700

    Finish sign-out functionality

--- app/src/main/java/ssd/project/mickeycj/todosapp/MainActivity.java
+++ app/src/main/java/ssd/project/mickeycj/todosapp/MainActivity.java
@@ -1,15 +1,18 @@
 package ssd.project.mickeycj.todosapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView usernameTextView;
-    private Button helpButton;
+    private Button helpButton, viewTodosButtons, viewProfileButton, signOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +27,42 @@ public class MainActivity extends AppCompatActivity {
 
         helpButton = (Button) findViewById(R.id.button_help);
         helpButton.setOnClickListener(onHelpClickListener);
+        viewTodosButtons = (Button) findViewById(R.id.button_view_todos);
+        viewTodosButtons.setOnClickListener(onViewTodosClickListener);
+        viewProfileButton = (Button) findViewById(R.id.button_view_profile);
+        viewProfileButton.setOnClickListener(onViewProfileClickListener);
+        signOutButton = (Button) findViewById(R.id.button_sign_out);
+        signOutButton.setOnClickListener(onSignOutClickListener);
     }
 
     private View.OnClickListener onHelpClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 
+        }
+    };
+
+    private View.OnClickListener onViewTodosClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+    private View.OnClickListener onViewProfileClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+    private View.OnClickListener onSignOutClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(MainActivity.this, SignInActivity.class));
+            finish();
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
         }
     };
 }
