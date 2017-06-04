ac990a27be3bf3b39a886ce610ab531769962a6d
Chanon Jenakom <chanonjenakom@gmail.com>
2017-06-04 16:56:29 +0700

    Finish view for item list activity

--- app/src/main/java/ssd/project/mickeycj/todosapp/ItemListActivity.java
+++ app/src/main/java/ssd/project/mickeycj/todosapp/ItemListActivity.java
@@ -3,6 +3,7 @@ package ssd.project.mickeycj.todosapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
@@ -13,9 +14,13 @@ import java.util.List;
 
 import ssd.project.mickeycj.todosapp.model.Item;
 import ssd.project.mickeycj.todosapp.model.User;
+import ssd.project.mickeycj.todosapp.view.OnViewHolderClickListener;
+import ssd.project.mickeycj.todosapp.view.adapter.ItemListAdapter;
+import ssd.project.mickeycj.todosapp.view.dialog.OptionsDialog;
 
 public class ItemListActivity extends AppCompatActivity {
 
+    private int todoIndex;
     private List<Item> itemList;
 
     private TextView usernameAppBarTextView, ownerTextView;
@@ -29,8 +34,9 @@ public class ItemListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
 
+        todoIndex = getIntent().getIntExtra("todoIndex", 0);
         itemList = new ArrayList<>();
-        // itemListAdapter
+        itemListAdapter = new ItemListAdapter(itemList, onItemClickListener);
 
         initViewHolders();
 
@@ -50,7 +56,49 @@ public class ItemListActivity extends AppCompatActivity {
         newItemButton.setOnClickListener(onNewItemClickListener);
         backButton = (Button) findViewById(R.id.button_back_from_todo_list);
         backButton.setOnClickListener(onBackClickListener);
+
+        itemListRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_item_list);
+        itemListRecyclerView.setLayoutManager(new LinearLayoutManager(ItemListActivity.this, LinearLayoutManager.VERTICAL, false));
+        itemListRecyclerView.setHasFixedSize(true);
+        itemListRecyclerView.setAdapter(itemListAdapter);
     }
+
+    private OnViewHolderClickListener onItemClickListener = new OnViewHolderClickListener() {
+        @Override
+        public void onItemClick(View view, int position) {
+            final OptionsDialog optionsDialog = new OptionsDialog(ItemListActivity.this);
+            View.OnClickListener onMarkItemClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    updateItemList();
+                    optionsDialog.dismiss();
+                }
+            };
+            View.OnClickListener onEditItemClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    finish();
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                }
+            };
+            View.OnClickListener onDeleteItemClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    updateItemList();
+                    optionsDialog.dismiss();
+                }
+            };
+            optionsDialog
+                    .setOptionsTitle(String.format(getString(R.string.options_title), "ITEM"))
+                    .setFirstOptionButton(getString(R.string.option_mark_item), onMarkItemClickListener)
+                    .setSecondOptionButton(getString(R.string.option_edit_item), onEditItemClickListener)
+                    .setThirdOptionButton(getString(R.string.option_delete_item), onDeleteItemClickListener)
+                    .show();
+        }
+    };
 
     private View.OnClickListener onHelpClickListener = new View.OnClickListener() {
         @Override
@@ -80,7 +128,11 @@ public class ItemListActivity extends AppCompatActivity {
     }
 
     private void updateItemList() {
+        if (!itemList.isEmpty()) {
+            itemList.clear();
+        }
 
+        itemListAdapter.notifyDataSetChanged();
     }
 
     @Override
